6f2d22df79a8e62be73f385d19df2487b1812681
Nicholas Rishel <rishel.nick@gmail.com>
2020-10-29 21:30:26 -0700

    Add namespace to library names to prevent name clash with system libraries.

--- build/build.rs
+++ build/build.rs
@@ -26,15 +26,15 @@ fn main() {
 
     // Link order matters, make sure dependents are linked before their dependencies.
     println!("cargo:rustc-link-search={}", bin_dir.to_str().unwrap());
-    println!("cargo:rustc-link-lib=glslang");
-    println!("cargo:rustc-link-lib=HLSL");
-    println!("cargo:rustc-link-lib=OGLCompiler");
-    println!("cargo:rustc-link-lib=OSDependent");
-    println!("cargo:rustc-link-lib=SPIRV");
-    println!("cargo:rustc-link-lib=SPVRemapper");
+    println!("cargo:rustc-link-lib=glslang.glsltospirv");
+    println!("cargo:rustc-link-lib=HLSL.glsltospirv");
+    println!("cargo:rustc-link-lib=OGLCompiler.glsltospirv");
+    println!("cargo:rustc-link-lib=OSDependent.glsltospirv");
+    println!("cargo:rustc-link-lib=SPIRV.glsltospirv");
+    println!("cargo:rustc-link-lib=SPVRemapper.glsltospirv");
     if target.contains("x86_64-unknown-linux-gnu") || target.contains("x86_64-apple-darwin") {
-        println!("cargo:rustc-link-lib=SPIRV-Tools-opt");
-        println!("cargo:rustc-link-lib=SPIRV-Tools");
+        println!("cargo:rustc-link-lib=SPIRV-Tools-opt.glsltospirv");
+        println!("cargo:rustc-link-lib=SPIRV-Tools.glsltospirv");
     }
     if target.contains("android") {
         println!("cargo:rustc-link-lib=c++_shared");
