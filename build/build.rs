c0fd726d0b09b52cf263786d4c05b8a2237fa7fb
GrygrFlzr <grygrflzr@hotmail.com>
2021-01-04 21:10:55 +0700

    Build i686-pc-windows-msvc libraries from source
    Increment version to 0.2.2
    Add dependency on cmake and cc crates
    Updated readme with additional instructions

--- build/build.rs
+++ build/build.rs
@@ -1,5 +1,19 @@
+extern crate cmake;
+
 use std::env;
+use std::fs::copy;
+use std::path::Path;
 use std::path::PathBuf;
+use std::process::Command;
+
+const COMMON_FILES: &[&str] = &[
+    "glslang",
+    "HLSL",
+    "OGLCompiler",
+    "OSDependent",
+    "SPIRV",
+    "SPVRemapper",
+];
 
 fn main() {
     let target = env::var("TARGET").unwrap();
@@ -8,6 +22,9 @@ fn main() {
 
     if target.contains("x86_64-pc-windows-msvc") {
         bin_dir.push("windows");
+    } else if target.contains("i686-pc-windows-msvc") {
+        build_windows_i686();
+        bin_dir.push("windows-i686");
     } else if target.contains("x86_64-unknown-linux-gnu") {
         bin_dir.push("linux");
     } else if target.contains("x86_64-apple-darwin") {
@@ -39,4 +56,56 @@ fn main() {
     if target.contains("android") {
         println!("cargo:rustc-link-lib=c++_shared");
     }
+}
+
+fn build_windows_i686() {
+    // Prepare directories
+    let cargo_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let source_dir = cargo_dir.join("glslang");
+    let install_dir = source_dir.join("install");
+    let library_source = install_dir.join("lib");
+    let library_destination = cargo_dir.join("build").join("windows-i686");
+
+    // Re-use libraries if they exist
+    if library_destination.exists() {
+        return;
+    }
+
+    // Initialize submodules
+    Command::new("git")
+        .args(&["submodule", "update", "--init"])
+        .status()
+        .expect("Failed to update submodules.");
+
+    // Set up "install" subdirectory
+    match std::fs::create_dir_all(&install_dir) {
+        Ok(_) => {}
+        Err(err) => panic!("Unable to create directory: {:?}", err),
+    }
+
+    // Configure and run build
+    cmake::Config::new(&source_dir)
+        .define("CMAKE_INSTALL_PREFIX", &install_dir)
+        .define("ENABLE_GLSLANG_BINARIES", "OFF")
+        .profile("Release")
+        .build_target("install")
+        .build();
+
+    // Copy library files to /build
+    match std::fs::create_dir_all(&library_destination) {
+        Ok(_) => {}
+        Err(err) => panic!("Unable to create directory: {:?}", err),
+    }
+
+    COMMON_FILES.iter().for_each(|file| {
+        match copy(
+            library_source.join(file).with_extension("lib"),
+            library_destination
+                .join(file)
+                .with_extension("glsltospirv.lib"),
+        ) {
+            Ok(_) => {}
+            Err(err) => panic!("Error copying glslang libaries: {}", err),
+        }
+    });
 }
--- readme.md
+++ readme.md
@@ -2,3 +2,17 @@ This crate is deprecated please use [shaderc-rs](https://github.com/google/shade
 
 
 BEVY NOTE: This crate is a temporary measure until native rust shader compilation like https://github.com/gfx-rs/naga lands.
+
+---
+
+## Additional Dependencies for `i686-pc-windows-msvc`
+
+Assuming an MSVC Windows host (either 32 or 64-bit):
+- git
+- [cmake](https://cmake.org/download/)
+- [VS C++ Build Tools](https://aka.ms/buildtools) (2017 or higher)
+  - Select **Visual C++ build tools**
+  - Make sure **Visual C++ tools for CMake** is ticked on the right
+  - Restart the computer after installing build tools - will fail to build otherwise
+
+`glslang` will be built from source the first time. Compiled libraries are re-used afterwards.
